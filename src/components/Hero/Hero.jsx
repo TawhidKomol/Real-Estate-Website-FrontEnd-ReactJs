3afe21652dd9713ef3a03e918963f53c583f3c99
Tawhid-Komol <tawhid15-2861@diu.edu.bd>
2023-06-02 02:33:25 +0600

    sixth

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className="hero-wrapper">
+        <div className="paddings innerWidth flexCenter hero-container">
+            <div className="hero-left">
+                Left Section
+            </div>
+            <div className="flexCenter hero-right">
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
